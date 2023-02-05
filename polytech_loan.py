d376669314a754daaadc335ee962a641ced40011
dattlab <122553391+dattlab@users.noreply.github.com>
2023-02-06 02:32:29 +0800

    Add navigation methods

--- polytech_loan.py
+++ polytech_loan.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import (
 from PyQt5.uic import loadUi
 
 from ui.main_window_ui import Ui_MainWindow
+from utils.error_handlers import *
 
 
 class Window(QMainWindow, Ui_MainWindow):
@@ -16,6 +17,28 @@ class Window(QMainWindow, Ui_MainWindow):
     def raiseInputError(self):
         dialog = errorInputDialog(self)
         dialog.exec()
+
+    def raiseApplySuccess(self):
+        dialog = applySuccessDialog(self)
+        dialog.exec()
+
+    def gotoDashboardPage1(self):
+        pass
+
+    def gotoDashboardPage2(self):
+        pass
+
+    def gotoApplyPage1(self):
+        pass
+
+    def gotoApplyPage2(self):
+        pass
+
+    def gotoSummaryPage(self):
+        pass
+
+    def gotoLoginPage(self):
+        pass
 
 
 class errorInputDialog(QDialog):
