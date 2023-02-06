c6da5bd9cf3f652a5ba82174aaffe354e60635bf
dattlab <122553391+dattlab@users.noreply.github.com>
2023-02-06 20:15:22 +0800

    Update createPdf() and requirements.txt

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,11 +1,40 @@
+from fpdf import FPDF
+
+from utils.constants import INTERFACE_FONT
+
+
 def exceedsMaxAmount():
     # TODO: Check loan amount input function
     pass
 
 
-def createPdf():
-    # TODO: Pdf creator function
-    pass
+def createPdf(filePath):
+    pdf = FPDF()
+    pdf.set_font(INTERFACE_FONT, size=15)
+    pdf.multi_cell(
+        200, 10,
+        "**Polytech-Loan**\n"
+        "**Loan Summary Details**\n\n" +
+        "                                                   **Iskolar Details**\n"
+        f"**Name:** {}\n" +
+        f"**Email:** {}\n" +
+        f"**Student Number:** {}\n" +
+        f"**College:** {}\n" +
+        f"**Course:** {}\n" +
+        f"**GWA:** {}\n" +
+        f"**Honor:** {}\n\n" +
+        "                                                   **Loan Details**\n"
+        f"**Loan amount:** {}\n" +
+        f"**Interest amount:** {}\n" +
+        f"**Payment duration:** {}\n" +
+        f"**Total debt:** {}\n" +
+        f"**Monthly payment:** {}\n" +
+        f"**Mode of payment:** {}\n" +
+        f"**Purpose:** {}\n\n",
+        align='L',
+        markdown=True
+    )
+    pdf.output(filePath)
 
 
 def storeInDB():
