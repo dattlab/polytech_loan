b3dd7fed05788fdcb7fa10345a1ac3e76b064690
dattlab <122553391+dattlab@users.noreply.github.com>
2023-02-06 14:27:52 +0800

    Add isFloat() checker

--- utils/error_handlers.py
+++ utils/error_handlers.py
@@ -7,9 +7,17 @@ def isNotEmpty(*args):
 
 def isInDB():
     # TODO: Check DB function
-    return True
+    return False
 
 
 def gwaAccepted(gwa):
     # TODO: Check GWA function
+    return True
+
+
+def isFloat(userInput):
+    try:
+        float(userInput)
+    except ValueError:
+        return False
     return True
