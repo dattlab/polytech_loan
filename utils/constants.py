e0001e4d783fbd067731b5fc041d2af9b9e03d2e
dattlab <122553391+dattlab@users.noreply.github.com>
2023-02-09 01:51:12 +0800

    Change path

--- utils/constants.py
+++ utils/constants.py
@@ -1,13 +1,29 @@
+import getpass
+import sys
+import os
+
+
 # DATA FILE PATH
-DATA_FILE = "data.json"
+USER_NAME = getpass.getuser()
+if os.name == "nt":
+    DATA_FILE_PATH = f"C:\\Users\\{USER_NAME}\\AppData\\Local\\Polytech-Loan"
+    DATA_FILE = f"{DATA_FILE_PATH}\\data.json"
+else:
+    DATA_FILE_PATH = f"/home/{USER_NAME}/.local/share/Polytech-Loan"
+    DATA_FILE = f"/home/{USER_NAME}/.local/share/Polytech-Loan/data.json"
+
+if not os.path.exists(DATA_FILE_PATH):
+    os.makedirs(DATA_FILE_PATH)
 
 # DIALOG UI FILES PATH
-ERROR_INPUT_DIALOG_UI = "ui/error_input.ui"
-APPLY_SUCCESS_DIALOG_UI = "ui/apply_success_dialog.ui"
-APPLY_REJECTED_DIALOG_UI = "ui/apply_rejected_dialog.ui"
-EXCEED_MAX_AMOUNT_DIALOG_UI = "ui/exceed_max_amount_dialog.ui"
-INVALID_EMAIL_DIALOG_UI = "ui/invalid_email_dialog.ui"
-INVALID_CRED_DIALOG_UI = "ui/invalid_cred_dialog.ui"
+bundle_dir = getattr(sys, "_MEIPASS", os.path.abspath(os.path.dirname(__file__)))
+
+ERROR_INPUT_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "../ui/error_input.ui"))
+APPLY_SUCCESS_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "../ui/apply_success_dialog.ui"))
+APPLY_REJECTED_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "../ui/apply_rejected_dialog.ui"))
+EXCEED_MAX_AMOUNT_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "../ui/exceed_max_amount_dialog.ui"))
+INVALID_EMAIL_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "../ui/invalid_email_dialog.ui"))
+INVALID_CRED_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "../ui/invalid_cred_dialog.ui"))
 
 INTERFACE_FONT = "Helvetica"
 BG_MAROON_0 = "rgb(91, 6, 22)"
