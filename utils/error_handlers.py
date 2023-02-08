fa150ae73f6c7b4a8969e53684b3219f564dbc90
dattlab <122553391+dattlab@users.noreply.github.com>
2023-02-09 01:54:51 +0800

    Refactor dialog classes

--- utils/error_handlers.py
+++ utils/error_handlers.py
@@ -36,8 +36,7 @@ def validCredentials(studentNumber, *args):
         accounts = json.load(f)
     for a in args:
         if accounts[studentNumber][a[0]] != a[1]:
-            invalidCred = invalidCredentials()
-            invalidCred.exec()
+            invalidCredentials().exec()
             return False
     return True
 
@@ -60,5 +59,5 @@ def isValidEmail(email):
     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     if re.fullmatch(regex, email):
         return True
-    invalidEmailError().raiseError()
+    invalidEmailError().exec()
     return False
