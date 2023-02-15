d371bb26070b567d2eb084e60f0d6610a14e100c
dattlab <122553391+dattlab@users.noreply.github.com>
2023-02-15 21:56:56 +0800

    Change ui file path

--- utils/constants.py
+++ utils/constants.py
@@ -17,12 +17,12 @@ if not os.path.exists(DATA_FILE_PATH):
 # DIALOG UI FILES PATH
 bundle_dir = getattr(sys, "_MEIPASS", os.path.abspath(os.path.dirname(__file__)))
 
-ERROR_INPUT_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "../ui/error_input.ui"))
-APPLY_SUCCESS_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "../ui/apply_success_dialog.ui"))
-APPLY_REJECTED_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "../ui/apply_rejected_dialog.ui"))
-EXCEED_MAX_AMOUNT_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "../ui/exceed_max_amount_dialog.ui"))
-INVALID_EMAIL_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "../ui/invalid_email_dialog.ui"))
-INVALID_CRED_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "../ui/invalid_cred_dialog.ui"))
+ERROR_INPUT_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "error_input.ui"))
+APPLY_SUCCESS_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "apply_success_dialog.ui"))
+APPLY_REJECTED_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "apply_rejected_dialog.ui"))
+EXCEED_MAX_AMOUNT_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "exceed_max_amount_dialog.ui"))
+INVALID_EMAIL_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "invalid_email_dialog.ui"))
+INVALID_CRED_DIALOG_UI = os.path.abspath(os.path.join(bundle_dir, "invalid_cred_dialog.ui"))
 
 INTERFACE_FONT = "Helvetica"
 BG_MAROON_0 = "rgb(91, 6, 22)"
